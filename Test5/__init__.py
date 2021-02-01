4cced41d4a527e7666f89e77e12464b6fa855ad4
Eric <41087951+EricWu2003@users.noreply.github.com>
2021-02-01 15:37:53 -0500

    Add files via upload
    We now use Github Gists instead of pastebin to store used codes.

--- Test5/__init__.py
+++ Test5/__init__.py
@@ -13,8 +13,8 @@ probabilities = (0.5, 0.5)
 assert sum(probabilities) == 1
 assert len(prizes) == len(probabilities)
 
-PASTEBIN_API_USER_KEY = "999a799fd183b4c140d4c0fafdd91448"
-PASTEBIN_API_DEV_KEY = "d114f27662bc159b55db39b96dc050f8"
+
+GITHUB_TOKEN = "717bf42aac79664efdc389e2b15002e41bd6d56d"
 
 
 hashes = requests.get("https://pastebin.com/raw/DUV5ztr1").text.split("\n")
@@ -25,30 +25,18 @@ hashes = [x.strip('\r') for x in hashes]
 
 
 def save_used_codes(used_codes):
-    data = {
-        'api_option': 'paste',
-        'api_dev_key':PASTEBIN_API_DEV_KEY,
-        'api_paste_code': json.dumps(used_codes, indent = 4, sort_keys=True),
-        'api_paste_name': "used_codes",
-        'api_paste_expire_date': 'N',
-        'api_user_key': PASTEBIN_API_USER_KEY,
-        'api_paste_format': 'json'
-    }
-  
-    r = requests.post("https://pastebin.com/api/api_post.php", data=data)
-    print("Paste send: ", r.status_code if r.status_code != 200 else "OK/200")
-    print("Paste URL: ", r.text)
+    #this uses github gists to save our used codes file
+
+    gist_id="9099922858882e94fdb16ca3c9293dfa"
+
+    content=json.dumps(used_codes, indent = 4, sort_keys=True)
+    filename = "gashapon_used_codes"
+    headers = {'Authorization': f'token {GITHUB_TOKEN}'}
+    r = requests.patch('https://api.github.com/gists/' + gist_id, data=json.dumps({'files':{filename:{"content":content}}}),headers=headers) 
+    print(r.json())
+
 def get_used_codes():
-    data = {
-        'api_option': 'list',
-        'api_dev_key':PASTEBIN_API_DEV_KEY,
-        'api_user_key': PASTEBIN_API_USER_KEY,
-        'api_results_limit': "1000"
-    }
-    r = requests.post("https://pastebin.com/api/api_post.php", data=data)
-    url = r.text.split('paste_url')[-2].strip(">").strip("/").strip("<")
-    
-    url = "https://pastebin.com/raw/" + url.split('/')[-1]
+    url = "https://gist.githubusercontent.com/EricWu2003/9099922858882e94fdb16ca3c9293dfa/raw/3907432ba280813e5c02beff484e84a21527ac9d/gashapon_used_codes"
     used_codes = json.loads(requests.get(url).text)
     return used_codes
 
