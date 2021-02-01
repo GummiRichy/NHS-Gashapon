07ac5940397bfbe555da4725576b7154fc1fd94f
Eric <41087951+EricWu2003@users.noreply.github.com>
2021-01-31 23:11:41 -0500

    Add files via upload
    As a test, there are two types of prizes: chocolates and starbursts.
    Python will now randomly pick one of the prizes if it is the first time a code is being used. Python will save used codes and their results.

--- Test5/__init__.py
+++ Test5/__init__.py
@@ -1,13 +1,23 @@
 
 from flask import Flask, request, send_from_directory, render_template, jsonify, make_response
 import os
+from hashlib import sha256
+import json
+import random
 
 app = Flask(__name__)
 
+prizes = ("Chocolate", "Starburst")
+probabilities = (0.5, 0.5)
+assert sum(probabilities) == 1
+assert len(prizes) == len(probabilities)
 
 
-#with open('index.html') as f:
-#        html_source_code = f.read()
+with open('static/hashes.txt') as f:
+    hashes = f.readlines()
+    hashes = [x.strip() for x in hashes]
+with open('static/used_codes.json') as f:
+    used_codes = json.load(f)
 
 
 @app.route("/")
@@ -16,28 +26,40 @@ def home():
 
 @app.route("/getcode", methods = ["POST"])
 def handle_code(): 
-    req = request.get_json()
+    code = request.get_json()
     
-    print(req)
-    print(type(req))
+    #print(req)
+    #print(type(req))
 
-    if not type(req) == type(""):
-        res = make_response(jsonify(4), 200)
+    if not type(code) == type(""):
+        res = make_response(jsonify(0), 200)
         return res
 
-    if not req.isdigit():
-        res = make_response(jsonify(4), 200)
+    if not code.isdigit():
+        res = make_response(jsonify(0), 200)
         return res
-
-    code = int(req)
-
 
     
-    if code %17 == 0:
-        res = make_response(jsonify(1), 200)
-        return res
+
+
+    if sha256(code.encode('utf-8')).hexdigest() in hashes:
+        if code not in used_codes.keys():
+            result = random.randint(1, len(prizes))
+
+            used_codes[code] = prizes[result - 1]
+
+            with open("static/used_codes.json", 'w') as f:
+                f.write(json.dumps(used_codes))
+
+            res = make_response(jsonify(result), 200)
+            return res
+        else:
+            result = prizes.index(used_codes[code]) + 1
+            result = -result
+            res = make_response(jsonify(result), 200)
+            return res
     else:
-        res = make_response(jsonify(4), 200)
+        res = make_response(jsonify(0), 200)
         return res
 
 
