0a2c4a0c34a305b881f80a468669f411a0f92f81
Eric <41087951+EricWu2003@users.noreply.github.com>
2021-02-01 14:43:22 -0500

    Add files via upload
    The python script has been changed to use pastebin as a method for storing used codes and retrieving hashes rather than local files.
    For some reason apache2 doesn't allow python to read and write local files
    Using pastebin also allows us to check used codes conveniently

--- Test5/__init__.py
+++ Test5/__init__.py
@@ -4,6 +4,7 @@ import os
 from hashlib import sha256
 import json
 import random
+import requests
 
 app = Flask(__name__)
 
@@ -12,10 +13,49 @@ probabilities = (0.5, 0.5)
 assert sum(probabilities) == 1
 assert len(prizes) == len(probabilities)
 
+PASTEBIN_API_USER_KEY = "999a799fd183b4c140d4c0fafdd91448"
+PASTEBIN_API_DEV_KEY = "d114f27662bc159b55db39b96dc050f8"
 
-with open('static/hashes.txt') as f:
-    hashes = f.readlines()
-    hashes = [x.strip() for x in hashes]
+
+hashes = requests.get("https://pastebin.com/raw/DUV5ztr1").text.split("\n")
+hashes = [x.strip('\r') for x in hashes]
+#print(hashes)
+
+
+
+
+def save_used_codes(used_codes):
+    data = {
+        'api_option': 'paste',
+        'api_dev_key':PASTEBIN_API_DEV_KEY,
+        'api_paste_code': json.dumps(used_codes, indent = 4, sort_keys=True),
+        'api_paste_name': "used_codes",
+        'api_paste_expire_date': 'N',
+        'api_user_key': PASTEBIN_API_USER_KEY,
+        'api_paste_format': 'json'
+    }
+  
+    r = requests.post("https://pastebin.com/api/api_post.php", data=data)
+    print("Paste send: ", r.status_code if r.status_code != 200 else "OK/200")
+    print("Paste URL: ", r.text)
+def get_used_codes():
+    data = {
+        'api_option': 'list',
+        'api_dev_key':PASTEBIN_API_DEV_KEY,
+        'api_user_key': PASTEBIN_API_USER_KEY,
+        'api_results_limit': "1000"
+    }
+    r = requests.post("https://pastebin.com/api/api_post.php", data=data)
+    url = r.text.split('paste_url')[-2].strip(">").strip("/").strip("<")
+    
+    url = "https://pastebin.com/raw/" + url.split('/')[-1]
+    used_codes = json.loads(requests.get(url).text)
+    return used_codes
+
+
+print("used codes: " + str(get_used_codes()))
+
+
 with open('static/used_codes.json') as f:
     used_codes = json.load(f)
 
@@ -48,8 +88,7 @@ def handle_code():
 
             used_codes[code] = prizes[result - 1]
 
-            with open("static/used_codes.json", 'w') as f:
-                f.write(json.dumps(used_codes))
+            save_used_codes(used_codes)
 
             res = make_response(jsonify(result), 200)
             return res
